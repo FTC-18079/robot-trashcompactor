7fd31303bf4ab6c29811a3c26895e0ace0a54d79
Nicolas Silveira <nicsilvc@gmail.com>
2024-02-19 13:01:15 -0800

    Fixed shooter control, added velocity tolerance when waiting for target velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/Shooter.java
@@ -1,57 +1,61 @@
 package org.firstinspires.ftc.teamcode.shooter;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
-import com.arcrobotics.ftclib.hardware.ServoEx;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RobotMap;
+
+import static org.firstinspires.ftc.teamcode.shooter.ShooterConstants.*;
 
 public class Shooter extends SubsystemBase {
     HardwareMap hardwareMap;
     Telemetry telemetry;
 
     private final MotorEx shooter;
-    private final ServoEx flick;
+    private final Servo flick;
 
     public Shooter(HardwareMap hardwareMap, Telemetry telemetry) {
         this.hardwareMap = hardwareMap;
         this.telemetry = telemetry;
 
         shooter = new MotorEx(hardwareMap, RobotMap.MOTOR_SHOOTER);
-        flick = new SimpleServo(hardwareMap, RobotMap.SERVO_FLICK, ShooterConstants.FLICK_MIN_ANGLE, ShooterConstants.FLICK_MAX_ANGLE);
+        flick = hardwareMap.get(Servo.class, RobotMap.SERVO_FLICK);
 
         setupShooter();
     }
 
     public void setupShooter() {
-        shooter.setVeloCoefficients(ShooterConstants.kP, ShooterConstants.kI, ShooterConstants.kD);
+        shooter.stopAndResetEncoder();
+        shooter.setVeloCoefficients(kP, kI, kD);
+        shooter.setFeedforwardCoefficients(kS, kV, kA);
         shooter.setRunMode(Motor.RunMode.VelocityControl);
         shooter.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        shooter.resetEncoder();
     }
 
     public void fire() {
-        flick.turnToAngle(ShooterConstants.FLICK_SHOOT_ANGLE);
+        flick.setPosition(FLICK_SHOOT_ANGLE);
     }
 
     public void rest() {
-        flick.turnToAngle(ShooterConstants.FLICK_REST_ANGLE);
+        flick.setPosition(FLICK_REST_ANGLE);
     }
 
     public void shoot() {
-        shooter.set(ShooterConstants.toTicksPerSec(ShooterConstants.SHOOTER_RPM));
+        shooter.setVelocity(toTicksPerSec(SHOOTER_RPM));
     }
 
     public void stop() {
-        shooter.set(0);
+        shooter.stopMotor();
     }
 
     public boolean reachedTargetVel() {
-        return getShooterVelocity() == ShooterConstants.SHOOTER_RPM;
+        telemetry.addData("Shooter Velocity", getShooterVelocity());
+        telemetry.update();
+        return Math.abs(SHOOTER_RPM - getShooterVelocity()) < SHOOTER_VELOCITY_TOLERANCE;
     }
 
     public double getShooterVelocity() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/ShooterConstants.java
@@ -1,19 +1,24 @@
 package org.firstinspires.ftc.teamcode.shooter;
 
 public class ShooterConstants {
+    // Shooter velocity control
     public static double kP = 1.0;
     public static double kI = 0.0;
     public static double kD = 0.0;
+    public static double kS = 0.0;
+    public static double kV = 1.0;
+    public static double kA = 0.0;
 
-    public static int FLICK_MIN_ANGLE = 0;
-    public static int FLICK_MAX_ANGLE = 90;
-    public static int FLICK_SHOOT_ANGLE = 80;
+    // Flick positions
+    public static int FLICK_SHOOT_ANGLE = 1;
     public static int FLICK_REST_ANGLE = 0;
-    public static double SHOOTER_RPM = 40.0;
 
-    public static final int SHOOTER_TICKS = 0;
+    // Shooter velocity
+    public static double SHOOTER_RPM = 40.0;
+    public static double SHOOTER_VELOCITY_TOLERANCE = 0.5;
+    public static final int SHOOTER_TICKS = 28;
 
     public static double toTicksPerSec(double rpm) {
-        return rpm / 60 * (double) SHOOTER_TICKS;
+        return (rpm / 60) * (double) SHOOTER_TICKS;
     }
 }
