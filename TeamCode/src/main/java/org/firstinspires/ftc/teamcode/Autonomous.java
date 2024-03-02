3c9cb2497ff086a79f27aac70d35915594cbb1b1
Nicolas Silveira <nicsilvc@gmail.com>
2024-03-01 19:40:18 -0800

    Added debug options to opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,21 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.util.TransferData;
 
-@TeleOp(name = "TeleOp", group = "OpModes")
-public class TeleOpMode extends LinearOpMode {
+public class Autonomous extends LinearOpMode {
     @Override
     public void runOpMode() {
-        Pose2d initialPose = TransferData.currentPose;
-        TeamRobot robot;
-
-        robot = new TeamRobot(
-                TeamRobot.OpModeType.TELEOP,
+        Pose2d initialPose = new Pose2d();
+        long waitMillis = 0;
+        final TeamRobot robot = new TeamRobot(
+                TeamRobot.OpModeType.AUTO,
                 hardwareMap,
                 telemetry,
                 gamepad1,
@@ -23,21 +19,17 @@ public class TeleOpMode extends LinearOpMode {
                 initialPose
         );
 
-        telemetry.addData("Single Gamepad Mode", "B");
+        // Add auto delay
         while (!isStarted()) {
-            if (gamepad1.b) {
-                robot = new TeamRobot(
-                        TeamRobot.OpModeType.TELEOP,
-                        hardwareMap,
-                        telemetry,
-                        gamepad1,
-                        gamepad1,
-                        initialPose
-                );
+            if (gamepad1.dpad_up) {
+                waitMillis += 100;
+            } else if (gamepad1.dpad_down) {
+                waitMillis -= 100;
             }
         }
 
         waitForStart();
+        sleep(waitMillis);
 
         // Run until end or stopped
         while (opModeIsActive() || !isStopRequested()) {
@@ -45,7 +37,7 @@ public class TeleOpMode extends LinearOpMode {
             robot.updateTelemetry();
         }
 
-        // Reset currentPose in case opmode stops early
+        // Save pose data
         TransferData.currentPose = robot.chassis.getPoseEstimate();
         robot.reset();
     }
